fca25c69ce0ef92acddeee1a7548ed745759f22b
arvin <arvinjunior@163.com>
2021-02-26 13:57:23 +0800

    File类的练习（展开文件夹并排序，以结构方式打印）（修正后）

--- learn10/src/FileTree/FileTree.java
+++ learn10/src/FileTree/FileTree.java
@@ -12,12 +12,14 @@ import java.util.List;
  */
 public class FileTree {
     private static final int LEVEL = 0;
+    private static final boolean LAST = false;
     private static File FILE = null;
-    private static int INDEX = 0;
+    private static final int INDEX = 0;
 
     private static final String A = "└ ─ ─ ─ ";
     private static final String B = "├ ─ ─ ─ ";
     private static final String C = "│       ";
+    private static final String D = "        ";
 
     /**
      * 把文件目录引入
@@ -35,35 +37,43 @@ public class FileTree {
      * 调用filePrint以打印文件目录
      */
     public void filePrint(){
-        filePrinter(FILE,LEVEL);
+        filePrinter(FILE,LEVEL,LEVEL,INDEX,LAST);
     }
 
     /**
      * 实现文件目录打印及目录名称排序
      */
-    public void filePrinter(File file, int level){
+    private void filePrinter(File file, int level, int parentLevel, int index, boolean last){
         // 打印“-”以表示目录结构
-        printLines(level);
+        printLines(level,parentLevel,index);
 
         System.out.println(file.getName());
 
+        // 在确认是文件夹后展开并排序
         if (file.isDirectory()) {
             File[] fileDetails = fileProcess(file);
             for (int i = 0; i < fileDetails.length; i++) {
                 if (fileDetails.length == 1){
-                    INDEX = -1;
+                    index = -1;
+                    parentLevel = level;
+                    last = true;
                 } else {
                     if (i == fileDetails.length - 1) {
-                        INDEX = -1;
+                        index = -1;
+                        last = true;
+                        parentLevel = level-1;
+                    } else {
+                        last = false;
+                        parentLevel = level;
                     }
                 }
-                filePrinter(fileDetails[i],level+1);
+                filePrinter(fileDetails[i],level+1,parentLevel,index,last);
             }
         }
     }
 
     /**
-     * 展开并处理文件夹
+     * 展开并排序文件夹
      * @param file
      * @return
      */
@@ -106,26 +116,25 @@ public class FileTree {
         return files;
     }
 
-    private static void printLines(int level){
+    private static void printLines(int level, int parentLevel, int index){
        if (level == 1){
-            if (INDEX == -1){
+            if (index == -1){
                 System.out.print(A);
             } else {
                 System.out.print(B);
             }
        } else if (level > 1){
-           if (INDEX == -1){
-               for (int i = 0; i < level - 1; i++) {
-                   System.out.print(C);
-               }
+           for (int i = 0; i < parentLevel; i++) {
+               System.out.print(C);
+           }
+           for (int i = 0; i < level - parentLevel - 1; i++) {
+               System.out.print(D);
+           }
+           if (index == -1){
                System.out.print(A);
            } else {
-               for (int i = 0; i < level - 1; i++) {
-                   System.out.print(C);
-               }
                System.out.print(B);
            }
        }
-       INDEX = 0;
     }
 }
